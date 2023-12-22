9e4bc9f82bf9c8c06498396a840642571fe0f52c
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2023-12-22 08:49:10 -0400

    imported packages and added prompt questions in index.js

---
+++ index.js
@@ -0,0 +1,120 @@
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const validator = require("validator");
+
+// Array of questions for user input, validated for empty responses and correct email format
+const promptQuestions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Title cannot be empty";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Provide a short description of your project:",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Description cannot be empty";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Provide installation instructions:",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Installation instructions cannot be empty";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Provide usage information and examples:",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Usage information cannot be empty";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "credits",
+        message: "Enter any credits or acknowledgements:",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Credit section cannot be empty";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "Provide contribution guidelines:",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Guidelines cannot be empty";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Provide instructions for running tests:",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Testing instructions cannot be empty";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub username:",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Username cannot be empty";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email address:",
+        validate: (input) => {
+          if (!validator.isEmail(input)) {
+            return "Invalid email";
+          }
+          return true;
+        },
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Choose a license for your project:",
+        choices: ["MIT", "Apache--2", "GPL--3", "None"],
+      },
+    ])
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
